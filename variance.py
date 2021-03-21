34a996168dde2350dac9c6b39076a4f2ed8367b9
Bo Cheng <bocheng1992@gmail.com>
2021-03-21 00:30:35 -0400

    previous one was poo

--- variance.py
+++ variance.py
@@ -11,42 +11,50 @@ def population_variance(some_list):
     this population_variance function accepts a list
     and returns the population variance of a list
     """
-    # first let's get the number of elements in the list:
     number_of_elements = 0
     sum_of_list = 0
     numerator = 0
 
     for i in some_list:
+
+        # first let's get the number of elements in the list:
         number_of_elements = number_of_elements + 1
-    # now we need the mean
+
+        # now we need the sum of the list
         sum_of_list = sum_of_list + i
 
-    # let's work on the numerator
-        numerator = (i - sum_of_list/number_of_elements) ** 2
+    for i in some_list:
+        # let's work on the numerator
+        numerator = numerator + ((i - (sum_of_list/number_of_elements)) ** 2)
 
     # now we arrive at the answer
     return numerator/number_of_elements
 
 
 # sample variance:
+
+
 def sample_variance(some_list):
     """
     this sample_variance function accepts a list
     and returns the sample variance of a list
     """
 
-    # first let's get the number of elements in the list:
     number_of_elements = 0
     sum_of_list = 0
     numerator = 0
 
     for i in some_list:
+
+        # first let's get the number of elements in the list:
         number_of_elements = number_of_elements + 1
-    # now we need the mean
+
+        # now we need the sum of the list
         sum_of_list = sum_of_list + i
 
-    # let's work on the numerator
-        numerator = (i - sum_of_list/number_of_elements) ** 2
+    for i in some_list:
+        # let's work on the numerator
+        numerator = numerator + ((i - (sum_of_list/number_of_elements)) ** 2)
 
     # now we arrive at the answer
     return numerator/(number_of_elements - 1)
