2a39b43ab98f103283415fa39d32e08be89c01b1
Bo Cheng <bocheng1992@gmail.com>
2021-03-21 00:07:52 -0400

    vairance functions
    pop and sample variance

---
+++ variance.py
@@ -0,0 +1,52 @@
+"""
+here we implement the function to return the variance of a list
+we will do it for both sample variance and population variance
+"""
+
+
+# population variance:
+
+def population_variance(some_list):
+    """
+    this population_variance function accepts a list
+    and returns the population variance of a list
+    """
+    # first let's get the number of elements in the list:
+    number_of_elements = 0
+    sum_of_list = 0
+    numerator = 0
+
+    for i in some_list:
+        number_of_elements = number_of_elements + 1
+    # now we need the mean
+        sum_of_list = sum_of_list + i
+
+    # let's work on the numerator
+        numerator = (i - sum_of_list/number_of_elements) ** 2
+
+    # now we arrive at the answer
+    return numerator/number_of_elements
+
+
+# sample variance:
+def sample_variance(some_list):
+    """
+    this sample_variance function accepts a list
+    and returns the sample variance of a list
+    """
+
+    # first let's get the number of elements in the list:
+    number_of_elements = 0
+    sum_of_list = 0
+    numerator = 0
+
+    for i in some_list:
+        number_of_elements = number_of_elements + 1
+    # now we need the mean
+        sum_of_list = sum_of_list + i
+
+    # let's work on the numerator
+        numerator = (i - sum_of_list/number_of_elements) ** 2
+
+    # now we arrive at the answer
+    return numerator/(number_of_elements - 1)
